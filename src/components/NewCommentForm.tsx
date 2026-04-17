00cd5a4ea6b0c71f251124df2628ce22f26f4c75
gustavo <gustavoalmeida5446@gmail.com>
2026-04-17 11:39:06 -0300

    apply requested changes

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -65,7 +65,7 @@ export const NewCommentForm: React.FC<Props> = ({ onSubmit }) => {
     setSubmitting(false);
 
     if (isSuccess) {
-      setValues(current => ({ ...current, body: '' }));
+      clearForm();
     }
   };
 
